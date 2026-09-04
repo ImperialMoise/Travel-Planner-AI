5a400f7c6cbe373b1eb9f62abd8a235682d1ed5f
ImperialMoise <foofoot37@gmail.com>
2026-09-04 15:29:09 +0200

    fix-android-system-bar-insets

--- mobile/android/app/src/main/java/com/imperialmoise/lafabriqueavoyages/MainActivity.java
+++ mobile/android/app/src/main/java/com/imperialmoise/lafabriqueavoyages/MainActivity.java
@@ -1,5 +1,19 @@
 package com.imperialmoise.lafabriqueavoyages;
 
+import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(
+            getWindow(),
+            true
+        );
+    }
+}
